e84a1cc1b000af5e9ecc7b515967aad2327dbff1
ngotienloc <ngotienlocf@gmail.com>
2026-05-17 16:30:59 +0700

    rewrite

---
+++ main/task/task_gps.c
@@ -0,0 +1,10 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "app_queues.h"
+#include "drivers/gps/gps.h"
+#include "config.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "types.h"
+#include <string.h>
+
