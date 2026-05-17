44713a8b3fedf0075c213290e56de1c90d0e7f85
ngotienloc <ngotienlocf@gmail.com>
2026-05-17 14:38:25 +0700

    change config gps

--- main/config.h
+++ main/config.h
@@ -2,11 +2,11 @@
 
 // GPS 
 #define CFG_GPS_UART_PORT       UART_NUM_1
-#define CFG_GPS_UART_TX_PIN     17    
-#define CFG_GPS_UART_RX_PIN     18    
+#define CFG_GPS_UART_TX_PIN     38  
+#define CFG_GPS_UART_RX_PIN     39   
 #define CFG_GPS_UART_BAUD       9600
 #define CFG_GPS_UART_BUF        512
-#define CFG_GPS_SENTENCE_QLEN   4    
+#define CFG_GPS_SENTENCE_QLEN   4      
 
 //Prio_task
 #define CFG_PRIO_GPS            5
